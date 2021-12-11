a4b7475ba119362eebd25770710ee8deb13f6036
Trixian <90383233+Trixian@users.noreply.github.com>
2021-12-11 15:04:59 +0100

    Create advent_day1.py

---
+++ advent_day1.py
@@ -0,0 +1,39 @@
+#Import document and convert to integer
+a_file = open('advent_day_one.txt',"r")
+#test if code works on known data from example:
+#a_file = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263 ]
+sweep_list = [int(data) for data in a_file]
+start_number = sweep_list[0]
+increase = 0
+decrease = 0
+
+#test to see if list works
+print(f"Test output: {sweep_list[0]}")
+print(f"Test output: {sweep_list[1]}")
+
+#the actual coding:
+"""
+How many measurements from a_file are larger than the previous measurement?
+"""
+counter = 0
+decrease_counter = 0
+increase_counter = ('counter', 'decrease_counter')
+#remember, the length of the list is 2000, the last index is 1999
+while counter < len(sweep_list)-1:
+	#if the item is smaller than the next item.
+    if sweep_list[counter] > sweep_list[counter+1]: 
+        print("decrease")
+   # else:
+    #    print("increase")
+        if 'decrease':
+        	decrease_counter += 1
+    counter += 1 #add one to counter and re-assign
+
+#Test if all variables are taken in to account (should be 2000): 
+print(f"Test total: {(counter) +1}") 
+#test if code counts decrease
+print(f"Test decrease: {decrease_counter}")
+#Final awnser:
+print(f"Total increases: {(counter)-(decrease_counter)}")
+
+
